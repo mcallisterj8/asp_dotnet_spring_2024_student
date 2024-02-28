bd0f5bc6eb765d0cb35e4dc12dc0b3d6cfb37b83
Jacob McAllister <datadeltaanalysis@protonmail.com>
2024-02-28 00:29:36 -0600

    Removed unneeded import
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

--- UserLoginApp/Controllers/AuthController.cs
+++ UserLoginApp/Controllers/AuthController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
-using UserLoginApp.Data;
 using UserLoginApp.Models;
 using System.Text.Json;
 
