bb789aa129f41ab9d1525f2459de13569f59ec0a
Jacob McAllister <mcallisterj@westerntc.edu>
2024-03-07 16:27:24 -0600

    Class update
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- UserLoginApp/ClientApp/src/app/auth/services/auth.service.ts
+++ UserLoginApp/ClientApp/src/app/auth/services/auth.service.ts
@@ -57,6 +57,7 @@ export class AuthService {
   }
   
   public logout(): Observable<any> {
+    console.log("logout()");
     return this._http.post<any>(`/api/auth/logout`, {})
             .pipe(tap(() => {
               // Remove the user from local storage
