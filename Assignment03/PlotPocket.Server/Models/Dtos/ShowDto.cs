5ed7a28b36f4596f23edd7a519d9523ac49e71be
Jacob McAllister <mcallisterj@westerntc.edu>
2024-03-21 16:03:11 -0500

    Update
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

---
+++ Assignment03/PlotPocket.Server/Models/Dtos/ShowDto.cs
@@ -0,0 +1,13 @@
+
+namespace PlotPocket.Server.Models.Dtos;
+
+public class ShowDto {
+    public int Id { get; set; } = 0;
+    public int ShowApiId { get; set; }
+    public string Type { get; set; }
+    public string Title { get; set; }
+    public DateTime? Date { get; set; }
+    public bool IsBookmarked { get; set; } = false;
+    public string PosterPath { get; set; }    
+
+}
