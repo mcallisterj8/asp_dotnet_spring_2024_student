8c1b09dc1e477000360169c71a5666271b2f0ab3
Jacob McAllister <mcallisterj@westerntc.edu>
2024-03-26 16:32:06 -0500

    End of class 3-26-2024
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- Assignment03/PlotPocket.Client/src/app/media-manager/components/trending-list/trending-list.component.ts
+++ Assignment03/PlotPocket.Client/src/app/media-manager/components/trending-list/trending-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { TrendingService } from '../../services/trending.service';
+import { Observable } from 'rxjs';
+import { Show } from '../../models/show';
 
 @Component({
   selector: 'app-trending-list',
@@ -6,17 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './trending-list.component.css',
 })
 export class TrendingListComponent implements OnInit {
-  
+  private _trendingService = inject(TrendingService);
   public title: string = '';
+  public trendingList$: Observable<Show[] | []> = this._trendingService.trendingList$;
   public currentFilter: 'allTrending' | 'trendingMovies' | 'trendingTvShows' =
     'allTrending';
 
     ngOnInit(): void {
-      
+      this.getAllTrending();
     }
 
     public getAllTrending(): void {
-
+      this.title = "Trending Movies and TV Shows";
+      this.currentFilter = 'allTrending';
+      this._trendingService.getAllTrending();
     }
 
     public getTrendingMovies(): void {
--- Assignment03/PlotPocket.Client/src/app/media-manager/services/trending.service.ts
+++ Assignment03/PlotPocket.Client/src/app/media-manager/services/trending.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Show } from '../models/show';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrendingService {
+  private _http = inject(HttpClient); // Will make async requests to server.
+
+  private _trendingListSubject: BehaviorSubject<Show[] | []> = new BehaviorSubject<Show[] | []>([] as Show[]);
+
+  public trendingList$: Observable<Show[] | []> = this._trendingListSubject.asObservable();
 
   constructor() { }
+
+  public getAllTrending(): void {
+    this._http.get<Show[]>(`/api/trending`).subscribe(showList => {
+      this._trendingListSubject.next(showList);
+    });
+  }
+
 }
