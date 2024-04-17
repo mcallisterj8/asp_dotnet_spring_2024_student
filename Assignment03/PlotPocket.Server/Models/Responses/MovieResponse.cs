0a8eb09f1b6c7b194bc4c8e72380eda98f1528d5
Jacob McAllister <datadeltaanalysis@protonmail.com>
2024-04-17 14:51:58 -0500

    Changed "Dates" to "Date" in the MovieResponse class.
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

--- Assignment03/PlotPocket.Server/Models/Responses/MovieResponse.cs
+++ Assignment03/PlotPocket.Server/Models/Responses/MovieResponse.cs
@@ -11,20 +11,20 @@ public class MovieResponse {
     */
 }
 
-public class Dates {
-   /*
-        TODO: 
-        Define the model based on the response from the Movie endpoints from
-        The Movie Database's API. Be sure to use the [JsonPropertyName("<property>")] attribue
-        above each of your C# properties to ensure that the JSON maps properly to your objects.
-    */
+public class Date {
+    /*
+         TODO: 
+         Define the model based on the response from the Movie endpoints from
+         The Movie Database's API. Be sure to use the [JsonPropertyName("<property>")] attribue
+         above each of your C# properties to ensure that the JSON maps properly to your objects.
+     */
 }
 
 public class Movie : ApiMediaItem {
-   /*
-        TODO: 
-        Define the model based on the response from the Movie endpoints from
-        The Movie Database's API. Be sure to use the [JsonPropertyName("<property>")] attribue
-        above each of your C# properties to ensure that the JSON maps properly to your objects.
-    */
+    /*
+         TODO: 
+         Define the model based on the response from the Movie endpoints from
+         The Movie Database's API. Be sure to use the [JsonPropertyName("<property>")] attribue
+         above each of your C# properties to ensure that the JSON maps properly to your objects.
+     */
 }
